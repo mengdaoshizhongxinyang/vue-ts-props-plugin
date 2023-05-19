c9163ce9d6a1fa35c7fcaa299d9a252a0aaa0f22
mengdaoshizhongxinyang <programmercyk@gmail.com>
2023-05-19 09:44:32 +0800

    refactor: modify vite-plugin function name

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -1,9 +1,9 @@
 import { type Plugin } from 'vite'
 import { extname } from 'path';
 import { transform } from "@vue-ts-props-plugin/core";
-export function VueJSXPropsPlugin():Plugin{
+export function VueTSPropsPlugin():Plugin{
   return {
-    name:"VueJSXPropsPlugin",
+    name:"VueTSPropsPlugin",
     async transform(_,id){
       const ext=extname(id)
       if(ext=='.tsx' || ext=='.ts'){
--- packages/vite/vite.config.ts
+++ packages/vite/vite.config.ts
@@ -1,14 +1,14 @@
 import swc from "rollup-plugin-swc3";
-import { VueJSXPropsPlugin } from "./src";
+import { VueTSPropsPlugin } from "./src";
 import { defineConfig } from "vite";
 import { resolve,extname } from 'path'
 export default defineConfig({
-  plugins: [VueJSXPropsPlugin(),swc({
+  plugins: [swc({
     include: /\.[mc]?[jt]sx?$/,
     exclude: /node_modules/,
     tsconfig: 'tsconfig.json',
     jsc: {}
-  })],
+  }),VueTSPropsPlugin()],
   build:{
     lib: {
       // Could also be a dictionary or array of multiple entry points
