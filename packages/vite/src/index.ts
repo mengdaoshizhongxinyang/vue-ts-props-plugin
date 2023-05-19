720f54882b6c142aaafc72ea7315b404a4120037
mengdaoshizhongxinyang <programmercyk@gmail.com>
2023-05-19 11:29:33 +0800

    refactor: supprt more vite version

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -1,16 +1,14 @@
-import { type Plugin } from 'vite'
+import { type Plugin, version } from 'vite'
 import { extname } from 'path';
 import { transform } from "@vue-ts-props-plugin/core";
-export function VueTSPropsPlugin():Plugin{
+export function VueTSPropsPlugin(): Plugin {
   return {
-    name:"VueTSPropsPlugin",
-    transform:{
-      order:"pre",
-      handler:async (_,id)=>{
-        const ext=extname(id)
-        if(ext=='.tsx' || ext=='.ts'){
-          return transform(id)
-        }
+    name: "VueTSPropsPlugin",
+    enforce: "pre",
+    transform: async (_, id) => {
+      const ext = extname(id)
+      if (ext == '.tsx' || ext == '.ts') {
+        return transform(id)
       }
     }
   }
